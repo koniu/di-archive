0d93a5d73e2b463d4f052ba06b10e1ac68d487bb
koniu <koniu@riseup.net>
2016-03-01 17:21:08 +0000

    js: clean-up the controller and add comments

--- static/controller.js
+++ static/controller.js
@@ -1,52 +1,65 @@
 $(document).ready(function() {
 
   function kill_audio() {
+    // stop audio
     $('audio').trigger("stop");
-    //r btn = $('audio').parent().parent().parent().find('.playbtn')
+    // remove player
     $('#player').remove()
+    // return buttons to 'play' icon
     $('.playbtn').html('&nbsp;<i class="fa fa-play"></i>&nbsp;')
-    $('.playbtn').closest('tr').removeClass('bg-success')
-    $('.showlink').show()
   }
 
-
-  function toggle_play(e, target) {
-    var isthisstopped = e.find('.fa-play').length
+  function toggle_play(e) {
+    // find if we're stopped or playing
+    var isthisstopped = $(this).find('.fa-play').length
+    // stop any other audio in any case
     kill_audio()
+    // start audio if we're not playing
     if (isthisstopped) {
-      // row.find('.showlink').hide()
+      // find target placement for the player
+      var row = $(this).closest('tr')
+      var target = row.find('.showlink').parent()
+      // insert div for the player
       var playerdiv = $('<div id="player"></div>').appendTo(target);
+      // insert audio into the div
       playerdiv.html('<audio controls autoplay></audio>');
-      e.closest('tr').find('.audiolink').each(function(l,e) {
+      // insert sources into the audio
+      $(this).closest('tr').find('.audiolink').each(function(l,e) {
         $('audio').append('<source src="'+e.href+'">')
       })
+      // show player
       $(playerdiv).show()
+      // start audio
       $('audio').trigger("play");
-      e.html('&nbsp;<i class="fa fa-stop"></i>&nbsp;')
+      // change button icon to 'stop'
+      $(this).html('&nbsp;<i class="fa fa-stop"></i>&nbsp;')
+      // set audio to hide and die if ended or problems
+      $('audio').on('error abort ended', kill_audio)
     }
   }
 
-
-  $('.playbtn').show()
-  $('.playbtn').click(function(e){
-    var row = $(this).closest('tr')
-    var target = row.find('.showlink').parent()
-    toggle_play($(this),target)
-  })
-  $('audio').on('error abort ended', kill_audio)
-
-  $('.showlink').hover(function() {
-    var thumb = $(this).closest('tr').find('img')
+  function bg_set(image) {
     var img = $('#background img')
-    img.attr('src', thumb.attr('src'))
+    img.attr('src', image)
     img.stop(true).fadeTo(2000, 0.3)
+  }
 
-  }, function(){
+  function bg_unset() {
     var img = $('#background img')
     img.stop(true).fadeOut(1000)
   }
+
+  // show play-buttons and bind click event
+  $('.playbtn').show()
+  $('.playbtn').click(toggle_play)
+
+  // use thumbnail as page background when hovering over show
+  $('.showlink').hover(
+    function() { bg_set($(this).closest('tr').find('img').attr('src')) },
+    bg_unset
   )
 
+  // popovers with show blurb when hovering over show
   $('body').on('mouseover', '.showlink', function() {
     var e = $(this);
     if (e.data('title') == undefined) {
@@ -57,7 +70,6 @@ $(document).ready(function() {
         var title = $(r).find('#title').text
         e.popover('destroy').popover({ html : true, trigger : 'hover', content: r, container: 'body'})
         if (e.is(':hover')) { e.popover('show') }
-       // $('#background img').attr('src', $(r)[4].src)
       })
     }
   })
