8765fbb557b2046b93083c1b5d7e366dcf375186
koniu <koniu@riseup.net>
2016-03-02 00:24:37 +0000

    scraper: switch to using rss feed as a source

--- scraper.py
+++ scraper.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python
 
-from BeautifulSoup import BeautifulSoup
-from HTMLParser import HTMLParser
 from dateutil import parser as date_parser
+from xml.etree import cElementTree as ET
+import os.path
+import urllib
 import re
-import requests
 import shelve
 import internetarchive
-import database
 import peewee
 
-PAGE_CACHE='db/cache_pages.shelve'
-SHOW_CACHE='db/cache_shows.shelve'
-ARCHIVE_CACHE='db/cache_archive.shelve'
+import database
 
-PAGE_RANGE = [1,2,3,10,20]
-PAGE_RANGE = range(1,40)
+IA_CACHE='db/ia.cache'
+RSS_CACHE='db/rss.xml'
+RSS_URL='http://www.dissidentisland.org/?feed=show'
 
+#{{{ Feed
+class Feed(object):
+    def __init__(self):
+        if not os.path.isfile(RSS_CACHE):
+            urllib.urlretrieve(RSS_URL, RSS_CACHE)
+        rss = ET.parse(RSS_CACHE)
+        self.xml_items = rss.find('channel').findall('item')
+
+    def get_items(self):
+        return [FeedItem(i) for i in self.xml_items]
+#}}}
+#{{{ FeedItem
+class FeedItem(object):
+    def __init__(self, xml_item):
+        self.title = xml_item.find('title').text
+        self.url = xml_item.find('guid').text
+        self.blurb = xml_item.find('description').text
+        self.ident = xml_item.find('enclosure').attrib['url'].split('/')[-2]
+        self.date = xml_item.find('pubDate').text
+#}}}
 ##{{{ CachedObject
 class CachedObject(object):
     def __init__(self, cache, key):
@@ -35,98 +53,15 @@ class CachedObject(object):
             cache[k] = self._cached_value
         cache.close()
 ##}}}
-##{{{ Scraper
-class Scraper(object):
-    def __init__(self, pages=range(1,50)):
-        self.scraped_shows = []
-        for i in pages:
-            page = PageScraper(i)
-            show_scrapes = page.scrape_shows()
-            self.scraped_shows.extend(show_scrapes)
-
-    def get_shows(self):
-        return [(s.ident, s.url, s.image, s.blurb, s.title) for s in self.scraped_shows]
-##}}}
-##{{{ PageScraper
-class PageScraper(CachedObject):
-    def _cache_cb(self, page):
-        base_url = "http://www.dissidentisland.org/archive/page/%s"
-        return requests.get(base_url % page).text
-
-    def __init__(self, page):
-        # get page content from cache or callback method
-        super(PageScraper, self).__init__(PAGE_CACHE, page)
-        self.soup = BeautifulSoup(self._cached_value)
-
-    def scrape_shows(self):
-        shows = []
-        soup_shows = self.soup.findAll('h3')
-        for s in soup_shows[5:]:
-            show_url = s.find('a')['href']
-            show = ShowScraper(show_url)
-            shows.append(show)
-        return shows
-##}}}
-##{{{ ShowScraper
-class ShowScraper(CachedObject):
-    def _cache_cb(self, url):
-        return requests.get(url).text
-
-    def __init__(self, url):
-        # get page text from cache or callback method
-        super(ShowScraper, self).__init__(SHOW_CACHE, url)
-        self.__page_text = self._cached_value
-        self.__soup = BeautifulSoup(self.__page_text)
-        self.url = url
-        self.image = self.get_image()
-        self.blurb = self.get_blurb()
-        self.title = self.get_title()
-        self.ident = self.__get_ident()
-
-    def get_image(self):
-        soup = self.__soup
-        try:
-            return soup.find('div', {'id': 'main-column' }).find('img')['src']
-        except TypeError:
-            return None
-
-    def get_title(self):
-        soup = self.__soup
-        title = soup.find('title').text
-        title = HTMLParser().unescape(title)
-        return title
-
-    def get_blurb(self):
-        soup = self.__soup.find('div', {'id': 'main-column'})
-        text = "".join(soup.findAll('p', text=True)).strip()
-        text = HTMLParser().unescape(text)
-        text = text.replace('LISTEN\nDOWNLOAD MP3\nDOWNLOAD OGG', '')
-        text = "<br>".join(text.split('\n'))
-        return text
-
-    def __get_ident(self):
-        try:
-            soup = BeautifulSoup(self.__page_text)
-            download_links = soup.findAll('li',
-                    {'class': re.compile('download-')})
-            download_url = download_links[0].find('a')['href']
-            ident = download_url.split('/')[-2]
-            return ident
-        except:
-            pass
-##}}}
-
 #{{{ Show
 class Show(CachedObject):
-    def __init__(self, ident, url, image, blurb, title):
-        super(Show, self).__init__(ARCHIVE_CACHE, ident)
-        self.orig_url = url
-        self.orig_image = image
-        self.orig_blurb = blurb
-        self.orig_title = title
-        self.ident = ident
+    def __init__(self, rss_item):
+        self.ident = rss_item.ident
+        self.rss_item = rss_item
+        super(Show, self).__init__(IA_CACHE, rss_item.ident)
         self.ia = self._cached_value
 
+        self.orig_url = rss_item.url
         self.audio = self.get_audio_urls()
         self.title = self.get_title()
         self.blurb = self.get_blurb()
@@ -139,10 +74,7 @@ class Show(CachedObject):
         self.tags = self.get_tags()
 
     def _cache_cb(self, ident):
-        return internetarchive.Item(ident)
-
-    # def get_urlbase(self):
-    #     return "%s//%s/%s/" % (self.ia.protocol, self.ia.server, self.ia.dir)
+        return internetarchive.Item(self.ident)
 
     def get_ia_url(self):
         return "https://archive.org/details/%s" % self.ident
@@ -158,7 +90,7 @@ class Show(CachedObject):
         try:
            blurb = self.ia.metadata['description']
         except KeyError:
-            blurb = self.orig_blurb
+            blurb = self.rss_item.blurb
         blurb = re.sub('<br *\/*>', '\n', blurb)
         blurb = re.sub('<\/*div *\/*>', '\n', blurb)
         blurb = re.sub('\n\n+', '\n', blurb)
@@ -179,30 +111,32 @@ class Show(CachedObject):
         if self.ia.exists and self.ia.identifier != 'None':
             return self.ia.metadata['title']
         else:
-            return self.orig_title
+            return self.rss_item.title
 
     def get_date(self):
         # try get the date from title
         try:
             t = re.sub(r'[Ee]pisode \d+', '', self.get_title())
             date = date_parser.parse(t, fuzzy=True).date()
+            return date
         except:
-            date = None
+            pass
 
         # fallback onto IA metadata
-        if not date:
+        try:
             pubdate = self.ia.metadata['publicdate']
             date = date_parser.parse(pubdate).date()
+            return date
+        except:
+            pass
 
         # return
-        return date
+        return None
 
     def get_thumb(self):
         thumbs = self.ia.get_files(formats=['JPEG Thumb'])
         if thumbs:
             return thumbs[0].url
-        elif self.orig_image:
-            return self.orig_image
         else:
             return "/static/dot.png"
 
@@ -212,37 +146,34 @@ class Show(CachedObject):
         for f in originals:
             if f.format == 'JPEG':
                 return f.url
-        # fall back on d*i web
-        return self.orig_image
+        return None
 
     def get_number(self):
         try:
             regex = re.search(r'[Ee]pisode (\d+)', self.title)
             return regex.group(1)
         except:
-            return ""
+            return None
 
 #}}}
 
 if __name__ == "__main__":
+    print "Getting info from D*I RSS..."
+    rss = Feed()
 
+    print "Getting info from archive.org..."
+    shows = []
+    for rss_item in rss.get_items():
+        show = Show(rss_item)
+        shows.append(show)
 
-    print "Scraping %d pages..." % len(PAGE_RANGE)
-    scraper = Scraper(pages=PAGE_RANGE)
-    scraped_shows = scraper.get_shows()
-
-    print "Retrieving data from archive.org and populating the db..."
-    database.db.connect()
-
-    for ident, url, image, blurb, title in scraped_shows:
+    print "Populating the database..."
+    for s in shows:
         try:
-            show = database.Show.get(orig_url = url)
+            show = database.Show.get(ident = s.ident)
         except database.Show.DoesNotExist:
-            # generate Show object (retrieves IA data and resolves)
-            s = Show(ident, url, image, blurb, title)
-
-            # populate db
-            show, created = database.Show.get_or_create(
+            # create a Show record
+            show = database.Show.create(
                 title = s.title,
                 image = s.image,
                 thumb = s.thumb,
@@ -252,9 +183,8 @@ if __name__ == "__main__":
                 ident = s.ident,
                 orig_url = s.orig_url
             )
-            print '-----', show.title
-            show.save()
-            print show.id
+
+            # create Audio records
             for a in s.audio:
                 # a == (f.format, f.url, f.size, f.name)
                 # FIXME: fuck these positional[2] args[1]
@@ -265,8 +195,8 @@ if __name__ == "__main__":
                     name = a[3],
                     size = a[2]
                 )
-                print audio.name, created
 
+            # create Tag records
             for t in s.tags:
                 tag, created = database.Tag.get_or_create(name = t)
                 try:
